c8b58feaccaa8bb02aa2926fa2724621bb4700fe
Rodevp <rodriguezrodrigor12@gmail.com>
2022-03-30 16:14:57 -0500

    conectando controlador con ruta orders

---
+++ controllers/orders.py
@@ -0,0 +1,11 @@
+class GetOrders :
+
+    def __init__(self):
+        pass
+
+    def get(self) :
+
+        return [{
+            "order-id": 0,
+            "order-quantity": 3
+        }]
--- routes/orders.py
+++ routes/orders.py
@@ -1,14 +1,14 @@
 from flask import Blueprint, jsonify
 
+#controller
+from controllers.orders import GetOrders
 
 orders = Blueprint('orders', __name__)
 
 @orders.route('/orders', methods=['GET'])
 def get_orders() :
     
-    data = [{
-        "order-id": 0,
-        "order-quantity": 3
-    }]
+    orders = GetOrders()
+    data = orders.get()
 
     return jsonify(data), 200
